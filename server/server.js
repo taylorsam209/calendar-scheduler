c9e6679abee1960edd7758526978c6b67c90f054
Taylor Sam <taylorsam209@yahoo.com>
2018-03-09 16:03:26 -0800

    Fixed environment variable config

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
-require('dotenv').config;
+require('dotenv').config();
 const express = require('express'),
     cors = require ('cors'),
     bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = 3017;
-console.log(3017);
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
